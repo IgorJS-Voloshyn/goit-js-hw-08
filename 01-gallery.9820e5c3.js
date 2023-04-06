55e4a489b96fa455f53509f9de30ca857c45a091
IgorJS-Voloshyn <113348628+IgorJS-Voloshyn@users.noreply.github.com>
2023-04-06 18:24:32 +0000

    Deploying to gh-pages from @ IgorJS-Voloshyn/goit-js-hw-08@6eb5eedf2d50890cf6c8f3ebfa00165357ef35d1 🚀

---
+++ 01-gallery.9820e5c3.js
@@ -0,0 +1,2 @@
+console.log("galleryItems"),console.log("this one for cimmit change");
+//# sourceMappingURL=01-gallery.9820e5c3.js.map
---
+++ 01-gallery.a72e01ca.js
@@ -0,0 +1,2 @@
+console.log("galleryItems"),console.log("this one for cimmit change");
+//# sourceMappingURL=01-gallery.a72e01ca.js.map
