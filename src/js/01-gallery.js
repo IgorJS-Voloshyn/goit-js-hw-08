b21bce64087f0308e100da6b127d6d2d50cfd0b8
IgorJS-Voloshyn <igorVoloshyn.js@gmail.com>
2023-04-10 13:08:00 +0300

    fixing hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,27 @@
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const gallery = document.querySelector('.gallery');
 
-console.log('galleryItems');
-console.log('this one for cimmit change');
+
+       const markup = galleryItems.map(({preview, original, description}) =>
+      `<li class="gallery__item">
+        <a href='${original}'class='gallery__link'>
+            <img class='gallery__image' src = '${preview}' alt = '${description}' data-source = '${original}' width= '100%'>
+        </a>
+        </li >`);
+    
+    gallery.insertAdjacentHTML('beforeend', markup.join(''));
+    
+  
+gallery.insertAdjacentHTML('beforeend', markup.join(''));
+    
+
+     new SimpleLightbox('.gallery a', {
+        captionsData: "alt",
+        captionDelay: 250,
+    });
+ 
